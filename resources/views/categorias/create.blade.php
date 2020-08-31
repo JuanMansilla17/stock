5e0cb0d7f84adc3828ee31ecb7df9b16f04afad8
JuanPMansilla <juampimansilla17@gmail.com>
2020-08-30 23:33:38 -0300

    se reutilizo la vista de agregar categoria

--- resources/views/categorias/create.blade.php
+++ resources/views/categorias/create.blade.php
@@ -1,47 +1,32 @@
-@extends("../layouts.plantillaCategorias")
+@extends("../layouts.plantilla")
 
 @section("cabecera")
 
-CATEGORÍAS
+AGREGAR CATEGORÍA
 
 @endsection
 
 
 @section("contenido")
 
-<form  method="post" action="/categorias">
+<div class="container mt-5">
 
-<table>
-    <tr>
-        <td>Nueva Categoría:</td>
-        <td>
+    <div class="row">
+        <div  class="col-12"> 
+            <form action="/categorias" method="POST">
+                <label class="texto" for="nuevaCategoria">Nueva Categoría:</label>
+                <input type="text" name="descripcion" class="form-control">
+                {{csrf_field()}}
 
-            <input type="text" name="descripcion">
-
-             {{csrf_field()}}
-
-        </td>
-    </tr>
-
-    <tr>
-    <td > 
-
-    <input type="submit" name="enviar" value="Enviar" aling="center">
-
-    </td>
-        <td>
-            <input type="reset" name="borrar" value="Borrar">
-        </td>
-    </tr>
-
-</table>
-
-
-</form>
+                <input type="submit" name="enviar" value="Enviar" class="boton btn btn-primary"> 
+            </form>
+        </div>
+    </div>
+</div>
 
 @if(count($errors)>0)
     @foreach($errors->all() as $error)
-        {{$error}}
+        <p class="mensajeError">{{$error}}</p>
     @endforeach
 @endif
 
